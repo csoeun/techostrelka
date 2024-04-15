68ece6fcdb4d5689f813a8fc4b2e5d18af3107e9
unknown <User@b104-02.sttec.net>
2024-04-15 16:51:48 +0300

    added handler

--- internal/handlers/profile.go
+++ internal/handlers/profile.go
@@ -2,12 +2,13 @@ package handlers
 
 import (
 	"database/sql"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Profile(db *sql.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
-
+		c.HTML(http.StatusOK, "profile.html", gin.h{})
 	}
 }
