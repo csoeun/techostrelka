18f5a4a6c2290200eb10e591080c6cc2b48366ff
unknown <User@b104-02.sttec.net>
2024-04-15 12:53:42 +0300

    gs

--- internal/handlers/signup.go
+++ internal/handlers/signup.go
@@ -1,6 +1,9 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -8,4 +11,19 @@ import (
 
 func Signup(c *gin.Context) {
 	c.HTML(http.StatusOK, "signup.html", gin.H{})
+}
+
+func SignupPOST(c *gin.Context) {
+	bts, err := io.ReadAll(c.Request.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	res := map[string]any{}
+	json.Unmarshal(bts, &res)
+
+	fmt.Println(res)
+
+	c.JSON(http.StatusOK, gin.H{})
 }
