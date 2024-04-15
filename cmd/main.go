9fd873e058aa101afb1024b3aaba4cb7ccdd9438
unknown <User@b104-02.sttec.net>
2024-04-15 10:16:18 +0300

    added server

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "hey")
+	})
+
+	r.Run(":8080")
+}
